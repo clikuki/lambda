8d840ce45568a59b985f8dbf2fd3b1a9e0e32f15
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-03-27 18:21:21 +0800

    renamed definitions to abstractions + applications are now tuple pairs

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-interface Definition {
+interface Abstraction {
 	param: string;
-	body: Application;
+	body: Term;
 }
-type Application = (Definition | string)[];
+type Term = Application | Abstraction | string;
+type Application = [Term, Term];
 
 class Program {
-	_tree: Application;
+	_tree: Term;
 	constructor(code: string) {
 		this._tree = parseString(code);
 	}
 
-	betaReduce(syntax: Application | Definition = this._tree) {
+	betaReduce(syntax: Term = this._tree) {
 		// TODO: Finish implementation of beta-reduction
 		if (syntax instanceof Array) {
 			// Dealing with application
-			let func: Definition | null = null;
+			let func: Abstraction | null = null;
 			for (let i = 0; i < syntax.length; i++) {
 				const term = syntax[i];
 
@@ -34,7 +35,7 @@ class Program {
 	}
 
 	// TODO: Finish implementation of alpha-conversion
-	alphaConversion(syntax: Application, term: Definition | string): Application {
+	alphaConversion(syntax: Application, term: Abstraction | string): Application {
 		return syntax;
 	}
 
@@ -44,8 +45,9 @@ class Program {
 }
 
 const func_char = "@";
-function parseString(code: string): Application {
-	const applicationFrame: Application = [];
+function parseString(code: string): Term {
+	let a: Term | null = null;
+	let b: Term | null = null;
 	for (let i = 0; i < code.length; i++) {
 		const char = code[i];
 		if (char === func_char) {
@@ -53,36 +55,50 @@ function parseString(code: string): Application {
 			const start = i + 3;
 			const end = code.length;
 
-			applicationFrame.push({
+			// All characters at this point will be consumed
+			const abstraction = {
 				param: code[i + 1],
 				body: parseString(code.slice(start, end)),
-			});
+			} satisfies Abstraction;
 
-			// All characters at this point have been consumed
-			break;
+			if (!a) a = abstraction;
+			else b = abstraction;
+
+			i = end;
 		} else if (char === "(") {
 			// Perform parse within bracket group, this usually occurs before function declarations
 			const start = i + 1;
 			const end = findBracketPair(code, i);
 
-			let applications = parseString(code.slice(start, end));
-			applicationFrame.push(...applications);
+			let term = parseString(code.slice(start, end));
+			if (!a) a = term;
+			else b = term;
 
 			i = end;
 		} else {
 			// Add single character as variable
-			applicationFrame.push(char);
+			if (!a) a = char;
+			else b = char;
+		}
+
+		if (b) {
+			// Group a and b into an application
+			a = [a, b];
+			b = null;
 		}
 	}
-	return applicationFrame;
+
+	if (!a) throw "Cannot parse empty string";
+	return a;
 }
-function stringifyTree(syntax: Application | Definition): string {
+function stringifyTree(syntax: Term): string {
 	let str = "";
-	if (syntax instanceof Array) {
+	if (typeof syntax === "string") str = syntax;
+	else if (syntax instanceof Array) {
 		// Dealing with application
 		for (const term of syntax) {
 			if (typeof term === "string") str += term;
-			else str += stringifyTree(term as Definition);
+			else str += stringifyTree(term as Abstraction);
 		}
 	} else {
 		// Dealing with function
@@ -110,5 +126,5 @@ const OR = "@f.@g.@x.@y.fxgxy";
 const AND = "@f.@g.@x.@y.fgxyy";
 
 const program = new Program(`(${IF})(${TRUE})xy`);
-program.betaReduce();
+// program.betaReduce();
 console.log(program.toString());
