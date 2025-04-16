d3913e8ec48bed609d0283fcd03359083b2c74c6
Lanz Vasquez <lanzvasquez24@gmail.com>
2025-04-16 20:01:29 +0800

    circumventing unused vars error

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,40 @@
-import { Lambda, code } from "./lambda.js";
+import { code } from "./lambda.js";
 import { Tromp } from "./tromp.js";
 
 // TODO: Add undo
 
-// Church Boolean
-const TRUE = "@x.@y.x";
-const FALSE = "@x.@y.y";
-const NOT = code`@f.f${FALSE}${TRUE}`;
-const OR = "@f.@g.ffg";
-const AND = "@f.@g.fgf";
+class Expr {
+	// Church Boolean
+	static TRUE = "@x.@y.x";
+	static FALSE = "@x.@y.y";
+	static NOT = code`@f.f${this.FALSE}${this.TRUE}`;
+	static OR = "@f.@g.ffg";
+	static AND = "@f.@g.fgf";
 
-// Church Numerals
-function getNumeral(n: number): string {
-	if (n < 0) throw new Error("n must be a nonnegative number");
+	// Church Numerals
+	static numeral(n: number): string {
+		if (n < 0) throw new Error("n must be a nonnegative number");
 
-	function build(n: number): string {
-		if (n === 1) return "fx";
-		return `f(${build(n - 1)})`;
+		let start = "";
+		let end = "";
+		for (let i = n; i > 0; i--) {
+			// Don't parenthesize inner most application
+			if (i === 1) start += "f";
+			else {
+				start += "f(";
+				end += ")";
+			}
+		}
+
+		return `@f.@x.${start}x${end}`;
 	}
-
-	return "@f.@x." + build(n);
+	static SUCC = "@n.@f.@x.f(nfx)";
+	static ADD = "@m.@n.@f.@x.mf(nfx)";
+	static MULT = "@m.@n.@f.m(nf)";
+	static EXP = "@b.@n.nb";
+	static PRED = "@n.@f.@x.n(@g.@h.h(gf))(@u.x)(@u.u)";
+	static SUB = code`@m.@n.n${this.PRED}m`;
 }
-const SUCC = "@n.@f.@x.f(nfx)";
-const ADD = "@m.@n.@f.@x.mf(nfx)";
-const MULT = "@m.@n.@f.m(nf)";
-const EXP = "@b.@n.nb";
-const PRED = "@n.@f.@x.n(@g.@h.h(gf))(@u.x)(@u.u)";
-const SUB = code`@m.@n.n${PRED}m`;
 
 const container = document.querySelector("main")!;
 const lambdaInput = document.querySelector("#lambda") as HTMLTextAreaElement;
@@ -34,19 +42,7 @@ lambdaInput.addEventListener("change", () => {
 	console.log(lambdaInput.value);
 });
 
-const tromp = new Tromp(
-	code`${ADD}${getNumeral(4)}${getNumeral(3)}`
-	// code`${getNumeral(4)}${SUCC}${getNumeral(3)}`
-	// code`${MULT}${getNumeral(4)}${getNumeral(3)}`
-	// code`${EXP}${getNumeral(5)}${getNumeral(4)}`
-	// code`${PRED}${getNumeral(5)}`
-	// code`${SUB}${getNumeral(20)}${getNumeral(20)}`
-
-	// code`${OR}(${NOT}(${OR}${FALSE}${TRUE}))(${AND}${TRUE}${TRUE})`
-	// code`(@x.x)${TRUE}`
-	// "(@x.xx)(@x.xx)"
-	// code`(@z.@y.y(z(@a.@b.ba)))(@y.y${TRUE}${FALSE})`
-);
+const tromp = new Tromp(code`${Expr.ADD}${Expr.numeral(4)}${Expr.numeral(3)}`);
 container.appendChild(tromp.svg);
 lambdaInput.value = tromp.lambdaTree.toString();
 
